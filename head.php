03341196db97f84162715cebde762dae86b0fdef
Tyler Young <tyler@x-plane.com>
2015-09-13 08:34:56 -0500

    Tore out the last remnants of brewery & attorney stuff

--- head.php
+++ head.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <?php if(is_post_type_archive(CI_BEER_TYPE) || is_post_type_archive(CI_STAFF_TYPE)) { ?>
+    <?php if(is_post_type_archive(CI_STAFF_TYPE)) { ?>
         <title><?php post_type_archive_title('All ', true); ?> | <?php bloginfo('name'); ?></title>
     <?php } else { ?>
         <title><?php wp_title('|', true, 'right'); ?></title>
--- page-header-archive.php
+++ page-header-archive.php
@@ -3,7 +3,7 @@
 $showTitle = ciGetNormalizedMeta( 'show_page_title', true );
 if( $showTitle ) { ?>
     <div class="page-header">
-        <?php if(is_post_type_archive(CI_BEER_TYPE) || is_post_type_archive(CI_STAFF_TYPE)) { ?>
+        <?php if(is_post_type_archive(CI_STAFF_TYPE)) { ?>
             <h1>Our <?php echo roots_title(); ?></h1>
         <?php } else { ?>
             <h1><?php echo roots_title(); ?></h1>
